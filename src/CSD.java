3b39136645ca6b6cfb18b3ab8fa0f59155a5ab53
sfz19839 <sfz19839@localhost>
2015-07-02 13:48:42 +0100

    updated prototype of interface

--- src/CSD.java
+++ src/CSD.java
@@ -1,9 +1,25 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.StringWriter;
+
+import javax.script.ScriptContext;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+import javax.script.SimpleScriptContext;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.CTabFolder;
 import org.eclipse.swt.custom.CTabItem;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
@@ -11,15 +27,23 @@ import org.eclipse.swt.layout.RowLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Spinner;
 import org.eclipse.swt.widgets.Text;
 
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+
 public class CSD {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ScriptException, IOException{
     	Display display = new Display();
         Shell shell = new Shell(display);
 
@@ -31,10 +55,14 @@ public class CSD {
         shell.setLayout(layout);
         
         
-        // creating intial tabs for operation selection
+        // creating initial tabs for operation selection
         CTabFolder folder = new CTabFolder(shell, SWT.TOP); // create a tab set
         GridData data = new GridData(SWT.FILL, SWT.FILL, true, true,2, 1);
         folder.setLayoutData(data);
+        
+        
+        // add tabs to the layout
+        // make these more descriptive
         CTabItem cTabItem1 = new CTabItem(folder, SWT.NONE);
         cTabItem1.setText("Read");
         CTabItem cTabItem2 = new CTabItem(folder, SWT.NONE);
@@ -47,77 +75,86 @@ public class CSD {
         cTabItem5.setText("Search");
         
         
-        
-        Text text = new Text(folder, SWT.BORDER); // new text element
-        text.setText("Hello"); // new variable text gets a value
-        cTabItem1.setControl(text); // add this to a tab
-
-
-
-        
-        // create a label and a button
-        Label label = new Label(shell, SWT.NONE);
-        label.setText("A label");
-        Button button = new Button(shell, SWT.PUSH);
-        button.setText("Press Me");
-
-        // create a new label that will span two columns
-        label = new Label(shell, SWT.BORDER);
-        label.setText("This is a label");
-        // create new layout data
-        label.setLayoutData(data);
-
-        // create a new label which is used as a separator
-        label = new Label(shell, SWT.SEPARATOR | SWT.HORIZONTAL);
-        
-        // create new layout data
-        data = new GridData(SWT.FILL, SWT.TOP, true, false);
-        data.horizontalSpan = 2;
-        label.setLayoutData(data);
-
-        // create a right aligned button
-        Button b = new Button(shell, SWT.PUSH);
-        b.setText("New Button");
-
-        data = new GridData(SWT.LEFT, SWT.TOP, false, false, 2, 1);
-        b.setLayoutData(data);
-        
-         // create a spinner with min value 0 and max value 1000
-        Spinner spinner = new Spinner(shell, SWT.READ_ONLY);
-        spinner.setMinimum(0);
-        spinner.setMaximum(1000);
-        spinner.setSelection(500);
-        spinner.setIncrement(1);
-        spinner.setPageIncrement(100);
-        GridData gridData = new GridData(SWT.FILL, SWT.FILL, true, false);
-        gridData.widthHint = SWT.DEFAULT;
-        gridData.heightHint = SWT.DEFAULT;
-        gridData.horizontalSpan = 2;
-        spinner.setLayoutData(gridData);
-
-        Composite composite = new Composite(shell, SWT.BORDER);
-        gridData = new GridData(SWT.FILL, SWT.FILL, true, false);
+        // composite allows me to use more then one item in my tab folder
+        Composite composite = new Composite(folder, SWT.NONE);
+        GridData gridData = new GridData(SWT.FILL, SWT.FILL, true, false); // must figure these variables out
         gridData.horizontalSpan = 2;
         composite.setLayoutData(gridData);
-        composite.setLayout(new GridLayout(1, false));
-
-        Text txtTest = new Text(composite, SWT.NONE);
-        txtTest.setText("Testing");
-        gridData = new GridData(SWT.FILL, SWT.FILL, true, false);
-        txtTest.setLayoutData(gridData);
-
-        Text txtMoreTests = new Text(composite, SWT.NONE);
-        txtMoreTests.setText("Another test");
+        composite.setLayout(new GridLayout(2, false)); // two columns
+        // addition of new items
+        new Label(composite, SWT.NONE).setText("Write view");
+        // make buttons in a row
         
-        Group group = new Group(shell, SWT.NONE);
-        group.setText("This is my group");
-        gridData = new GridData(SWT.FILL, SWT.FILL, true, false);
-        gridData.horizontalSpan = 2;
-        group.setLayoutData(gridData);
-        group.setLayout(new RowLayout(SWT.VERTICAL));
-        Text txtAnotherTest = new Text(group, SWT.NONE);
-        txtAnotherTest.setText("Another test");
+        
+        Button b = new Button(composite,SWT.PUSH);
+        b.setText("numbers.py");
+        b.addSelectionListener(new SelectionListener(){
+        	
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				System.out.println("called");
+				
+				
+				
+				
+				try{
+					 int number1 = 10;
+					 int number2 = 32;
+					 Process p = Runtime.getRuntime().exec("python python_code/numbers.py "+number1+" "+number2);
+					 BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
+					 int ret = new Integer(in.readLine()).intValue();
+					 System.out.println("value is : "+ret);
+				 }catch(Exception z){System.out.println(z);}
+				
+			}
 
+			@Override
+			public void widgetDefaultSelected(SelectionEvent e) {
+				// TODO Auto-generated method stub}
+			}});
+        
+        new Button(composite, SWT.BUTTON4).setText("button 4");
+        
+        
+        Button browse = new Button(shell, SWT.PUSH);
+		browse.setText("Browse...");
+		gridData = new GridData(GridData.FILL, GridData.CENTER, true, false);
+		gridData.horizontalIndent = 5;
+		browse.setLayoutData(gridData);
+		
+		// adding a browse and return function
+		browse.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent event) {
+				String fileName = new FileDialog(shell).open();
+				if (fileName != null) {
+					Image dogImage = new Image(display, fileName);
+				}
+			}
+		});
+		
+		// trying to run a simple python program
+		
+		
+		/*
+		b.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent event) {
+				
+				
+			}});
+		*/
+
+		// try running py4j
+		
+        
+        
+        
+        
+        
+        
+        // set control
+        cTabItem2.setControl(composite);
+        
+        // pack and load shell
         shell.pack();
         shell.open();
         while (!shell.isDisposed()) {
@@ -127,4 +164,8 @@ public class CSD {
         }
         display.dispose();
     }
-} 
+}
+
+class mybutton{
+	
+}
